c02728317dca5b20728779aedc42677f6090b89e
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-11-15 15:18:55 +0000

    Initial non-working attempt at using matter.js.

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
+import {
+  Bodies,
+  Engine,
+  Render,
+  World
+} from 'matter-js'
+
 export class Game extends Component {
+
+  constructor() {
+    super();
+    this.containerRef = React.createRef();
+    this.engine = Engine.create();
+    this.renderer = Render.create({
+      element: this.containerRef.current,
+      engine: this.engine
+    });
+    const ball = Bodies.circle(50, 50, 10, {
+      // render: {
+      //   sprite: {
+      //     texture: 'sprite.png'
+      //   }
+      // }
+    });
+    const box = Bodies.rectangle(0, 0, 100, 100, {
+      isStatic: true
+    });
+    World.add(this.engine.world, [ball, box])
+  }
+
   render() {
     return (
-      <img src="sprite.png" alt="sprite"/>
+      <div ref={this.containerRef} />
     );
   }
+
+  componentDidMount() {
+    Engine.run(this.engine);
+    Render.run(this.renderer);
+  }
+
 }
