908af86d96c90cd1de9d9e2f0e026978ed665ae4
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-11-15 13:19:57 +0000

    Render fixed-location circular sprite.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Game } from './Game';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-            Hello World.
+          <Game/>
         </header>
       </div>
     );
