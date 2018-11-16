d4daf98ed33369e6a5880a8aac1ad6a63e3061af
Jeremy Green <j.green@jeremygreen.me.uk>
2018-11-16 17:07:20 +0000

    Convert GameEngine into a class, adding Engine/Render start/stop code that was in Game Component.

--- src/Game.js
+++ src/Game.js
@@ -4,7 +4,7 @@ import {
   Engine,
   Render,
 } from 'matter-js'
-import { createEngine, createRenderer } from './GameEngine';
+import { GameEngine } from './GameEngine';
 
 export class Game extends Component {
 
@@ -12,9 +12,7 @@ export class Game extends Component {
 
   constructor(props) {
     super(props);
-    this.renderer = null;
-    this.bar = null;
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.gameEngine = null;
   }
 
   render() {
@@ -23,54 +21,16 @@ export class Game extends Component {
     );
   }
 
-  handleKeyPress(event) {
-    if (this.bar === null) {
-      return;
-    }
-    let deltaX = 0;
-    if (event.key === 'ArrowLeft') {
-      deltaX = -30;
-    } else if (event.key === 'ArrowRight') {
-      deltaX = 30;
-    }
-    if (deltaX !== 0) {
-      let x = this.bar.position.x + deltaX;
-      const minX = 100;
-      const maxX = 800 - 100;
-      Body.setPosition(this.bar, {
-        x: this.clamp(x, minX, maxX),
-        y: this.bar.position.y
-      });
-    }
-  }
-
-  clamp(x, min, max) {
-    if (x < min) {
-      x = min;
-    }
-    if (x > max) {
-      x = max;
-    }
-    return x;
-  }
-
-  // TODO encapsulate startEngine(), stopEngine() and associated state in class.
   startEngine() {
-    const { engine, bar } = createEngine(this.props.level);
-    this.bar = bar;
-    const container = document.getElementById(this.CONTAINER_ID);
-    this.renderer = createRenderer(container, engine);
-    Engine.run(engine);
-    Render.run(this.renderer);
-    document.addEventListener('keydown', this.handleKeyPress);
+    this.gameEngine = new GameEngine(this.CONTAINER_ID, this.props.level);
+    this.gameEngine.start();
   }
 
   stopEngine() {
-    document.removeEventListener('keydown', this.handleKeyPress);
-    Render.stop(this.renderer);
-    this.renderer.canvas.remove();
-    this.renderer = null;
-    this.bar = null;
+    if (this.gameEngine) {
+      this.gameEngine.stop();
+      this.gameEngine = null;
+    }
   }
 
   // React lifecycle.
@@ -80,9 +40,7 @@ export class Game extends Component {
 
   // React lifecycle.
   componentWillUnmount() {
-    if (this.renderer) {
-      this.stopEngine();
-    }
+    this.stopEngine();
   }
 
   // React lifecycle.
@@ -90,9 +48,7 @@ export class Game extends Component {
     if (this.props === prevProps) {
       return;
     }
-    if (this.renderer) {
-      this.stopEngine();
-    }
+    this.stopEngine();
     this.startEngine();
   }
 
--- src/GameEngine.js
+++ src/GameEngine.js
@@ -8,94 +8,143 @@ import {
 import _ from 'lodash';
 import seedrandom from 'seedrandom';
 
-export function createEngine(level) {
-  const engine = Engine.create();
-  engine.world.gravity.y = 0.2;
-  const ball = createBall();
-  const bar = createBar();
-  const walls = createWalls();
-  const obstacles = createObstacles(level);
-  World.add(engine.world, [
-    ...walls,
-    ball,
-    bar,
-    ...obstacles
-  ]);
-  return { engine, bar };
-}
+export class GameEngine {
 
-export function createRenderer(container, engine) {
-  return Render.create({
-    element: container,
-    engine: engine,
-    options: {
-      background: "transparent",
-      wireframes: false,
-      width: 800,
-      height: 600
-    }
-  });
-}
-
-function createWalls() {
-  const wallOptions = {
-    isStatic: true,
-    friction: 0
-  };
-  const walls = [
-    Bodies.rectangle(400, 0, 800, 50, { ...wallOptions }),
-    Bodies.rectangle(400, 600, 800, 50, { ...wallOptions }),
-    Bodies.rectangle(800, 300, 50, 600, { ...wallOptions }),
-    Bodies.rectangle(0, 300, 50, 600, { ...wallOptions })
-  ];
-  return walls;
-}
-
-function createBar() {
-  return Bodies.rectangle(100, 500, 100, 10, {
-    isStatic: true,
-    friction: 0
-  });
-}
-
-function createBall() {
-  const radius = 27;
-  const x = 100;
-  const y = radius;
-  // Infinite inertia reduces conversion of linear to angular
-  // momentum, making ball bounce longer:
-  //
-  // https://github.com/liabru/matter-js/issues/21#issuecomment-42775549
-  const ballInertia = Infinity;
-  const ball = Bodies.circle(x, y, radius, {
-    render: {
-      sprite: {
-        texture: 'ball.png'
+  constructor(containerId, level) {
+    this.container = document.getElementById(containerId);
+    this.engine = Engine.create();
+    this.engine.world.gravity.y = 0.2;
+    const ball = GameEngine.createBall();
+    this.bar = GameEngine.createBar();
+    const walls = GameEngine.createWalls();
+    const obstacles = GameEngine.createObstacles(level);
+    World.add(this.engine.world, [
+      ...walls,
+      ball,
+      this.bar,
+      ...obstacles
+    ]);
+    this.renderer = Render.create({
+      element: this.container,
+      engine: this.engine,
+      options: {
+        background: "transparent",
+        wireframes: false,
+        width: 800,
+        height: 600
       }
-    },
-    restitution: 1,
-    inertia: ballInertia,
-    inverseInertia: 1 / ballInertia,
-    friction: 0,
-    frictionAir: 0,
-    frictionStatic: 0,
-  });
-  Body.setVelocity(ball, { x: 0, y: 3 });
-  return ball;
-}
+    });
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
 
-function createObstacles(n) {
-  const random = seedrandom(n);
-  const obstacles = []
-  _.range(0, n).forEach(i => {
-    const x = random() * 800;
-    const y = random() * (0.8 * 600);
-    const radius = 10 + random() * 15;
-    const obstacle = Bodies.circle(x, y, radius, {
+  start() {
+    Engine.run(this.engine);
+    Render.run(this.renderer);
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  stop() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+    Render.stop(this.renderer);
+    this.renderer.canvas.remove();
+    this.container = null;
+    this.engine = null;
+    this.renderer = null;
+    this.bar = null;
+  }
+
+  handleKeyPress(event) {
+    if (this.bar === null) {
+      return;
+    }
+    let deltaX = 0;
+    if (event.key === 'ArrowLeft') {
+      deltaX = -30;
+    } else if (event.key === 'ArrowRight') {
+      deltaX = 30;
+    }
+    if (deltaX !== 0) {
+      let x = this.bar.position.x + deltaX;
+      const minX = 100;
+      const maxX = 800 - 100;
+      Body.setPosition(this.bar, {
+        x: this.clamp(x, minX, maxX),
+        y: this.bar.position.y
+      });
+    }
+  }
+
+  clamp(x, min, max) {
+    if (x < min) {
+      x = min;
+    }
+    if (x > max) {
+      x = max;
+    }
+    return x;
+  }
+
+  static createWalls() {
+    const wallOptions = {
+      isStatic: true,
+      friction: 0
+    };
+    const walls = [
+      Bodies.rectangle(400, 0, 800, 50, { ...wallOptions }),
+      Bodies.rectangle(400, 600, 800, 50, { ...wallOptions }),
+      Bodies.rectangle(800, 300, 50, 600, { ...wallOptions }),
+      Bodies.rectangle(0, 300, 50, 600, { ...wallOptions })
+    ];
+    return walls;
+  }
+
+  static createBar() {
+    return Bodies.rectangle(100, 500, 100, 10, {
       isStatic: true,
       friction: 0
     });
-    obstacles.push(obstacle);
-  });
-  return obstacles;
+  }
+
+  static createBall() {
+    const radius = 27;
+    const x = 100;
+    const y = radius;
+    // Infinite inertia reduces conversion of linear to angular
+    // momentum, making ball bounce longer:
+    //
+    // https://github.com/liabru/matter-js/issues/21#issuecomment-42775549
+    const ballInertia = Infinity;
+    const ball = Bodies.circle(x, y, radius, {
+      render: {
+        sprite: {
+          texture: 'ball.png'
+        }
+      },
+      restitution: 1,
+      inertia: ballInertia,
+      inverseInertia: 1 / ballInertia,
+      friction: 0,
+      frictionAir: 0,
+      frictionStatic: 0,
+    });
+    Body.setVelocity(ball, { x: 0, y: 3 });
+    return ball;
+  }
+
+  static createObstacles(n) {
+    const random = seedrandom(n);
+    const obstacles = []
+    _.range(0, n).forEach(i => {
+      const x = random() * 800;
+      const y = random() * (0.8 * 600);
+      const radius = 10 + random() * 15;
+      const obstacle = Bodies.circle(x, y, radius, {
+        isStatic: true,
+        friction: 0
+      });
+      obstacles.push(obstacle);
+    });
+    return obstacles;
+  }
+
 }
