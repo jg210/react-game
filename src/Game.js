cc9e8317589e97fbf795bf6e893051023d89d0d3
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-11-16 11:21:53 +0000

    Refactoring: move matter.js-related factory methods into new Engine.js file.

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-
 import {
-  Bodies,
   Body,
   Engine,
-  Events,
   Render,
-  World
 } from 'matter-js'
+import { createEngine, createRenderer } from './Engine';
 
 export class Game extends Component {
 
@@ -60,10 +57,10 @@ export class Game extends Component {
   }
 
   componentDidMount() {
-    const { engine, bar } = this.createEngine();
+    const { engine, bar } = createEngine();
     this.bar = bar;
     const container = document.getElementById(this.CONTAINER_ID);
-    this.renderer = this.createRenderer(container, engine);
+    this.renderer = createRenderer(container, engine);
     Engine.run(engine);
     Render.run(this.renderer);
     document.addEventListener('keydown', this.handleKeyPress);
@@ -75,95 +72,6 @@ export class Game extends Component {
     this.renderer = null;
     this.bar = null;
     document.removeEventListener('keydown', this.handleKeyPress);
-  }
-
-  createEngine() {
-    const engine = Engine.create();
-    engine.world.gravity.y = 0.2;
-    const ball = this.createBall();
-    const bar = this.createBar();
-    const walls = this.createWalls();
-    World.add(engine.world, [
-      ...walls,
-      ball,
-      bar
-    ]);
-    Events.on(engine, 'collisionStart', this.createCollisionHandler(ball));
-    return { engine, bar };
-  }
-
-  createRenderer(container, engine) {
-    return Render.create({
-      element: container,
-      engine: engine,
-      options: {
-        background: "transparent",
-        wireframes: false,
-        width: 800,
-        height: 600
-      }
-    });
-  }
-
-  createWalls() {
-    const wallOptions = {
-      isStatic: true,
-      friction: 0
-    };
-    const walls = [
-      Bodies.rectangle(400, 0, 800, 50, { ...wallOptions }),
-      Bodies.rectangle(400, 600, 800, 50, { ...wallOptions }),
-      Bodies.rectangle(800, 300, 50, 600, { ...wallOptions }),
-      Bodies.rectangle(0, 300, 50, 600, { ...wallOptions })
-    ];
-    return walls;
-  }
-
-  createBar() {
-    return Bodies.rectangle(100, 500, 100, 10, {
-      isStatic: true,
-      friction: 0
-    });
-  }
-
-  createBall() {
-    const radius = 27;
-    const x = 100;
-    const y = radius;
-    // Infinite inertia reduces conversion of linear to angular
-    // momentum, making ball bounce longer:
-    //
-    // https://github.com/liabru/matter-js/issues/21#issuecomment-42775549
-    const ballInertia = Infinity;
-    const ball = Bodies.circle(x, y, radius, {
-      render: {
-        sprite: {
-          texture: 'sprite.png'
-        }
-      },
-      restitution: 1,
-      inertia: ballInertia,
-      inverseInertia: 1 / ballInertia,
-      friction: 0,
-      frictionAir: 0,
-      frictionStatic: 0
-    });
-    return ball;
-  }
-
-  createCollisionHandler(ball) {
-    return event => {
-      event.pairs.forEach(pair => {
-        [pair.bodyA, pair.bodyB].forEach(body => {
-          if (body === ball) {
-            // Collisions in matter.js aren't 100% elastic, so
-            // invert the velocity when the ball hits something.
-            body.velocity.x = -body.velocity.x;
-            body.velocity.y = -body.velocity.y;
-          }
-        });
-      });
-    };
   }
 
 }
