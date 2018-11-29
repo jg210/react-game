38e65f3280b88ac77645e21a19b3db7774ac903c
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-11-29 17:28:17 +0000

    Move Components from App to GameScreen. Forget to git add saga files.

---
+++ src/GameScreen.js
@@ -0,0 +1,19 @@
+// @flow
+
+import React from 'react';
+
+import Game from './Game';
+import Level from './Level';
+import Scores from './Scores';
+
+type Props = {};
+
+export default (props: Props) => {
+  return (
+    <div>
+      <Scores />
+      <Game />
+      <Level numberOfLevels={10} />
+    </div>
+  );
+};
