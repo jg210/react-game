bf177b09e04175337ceb1d3a462a2f62c48f2db7
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-11-29 18:50:31 +0000

    Use index.js fro rootReducer.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@
 
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import { rootReducer } from "./reducers/rootReducer";
+import rootReducer from "./reducers";
 import sagas from './sagas';
 
 const sagaMiddleware = createSagaMiddleware()
