57150b1e1d840010faef139401e2c55bb46b66ca
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-12-08 17:34:17 +0000

    Don't show focus box in StartScreen.

--- src/screen/StartScreen.js
+++ src/screen/StartScreen.js
@@ -11,7 +11,11 @@ type Props = {
 
 const StartScreen = (props: Props) => {
   return (
-    <div tabIndex="0" onClick={props.startGame} onKeyPress={props.startGame} >
+    <div
+      tabIndex="0"
+      onClick={props.startGame}
+      onKeyPress={props.startGame}
+      className="StartScreen">
       <p>Press any key to start game.</p>
       <p>Use left and right arrows to position the magnet.</p>
       <p>Press space to turn the magnet on or off.</p>
