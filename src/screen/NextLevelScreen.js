8e0c377f6b2861bf92d29d90afb4a2457ff63fef
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-12-08 15:56:55 +0000

    Show level number in next-level screen.

--- src/screen/NextLevelScreen.js
+++ src/screen/NextLevelScreen.js
@@ -1,11 +1,24 @@
 // @flow
 
 import React from 'react';
+import { connect } from "react-redux";
+import type { LevelState } from '../redux/reducers/level';
 
-type Props = {};
+type Props = {
+  level: number
+};
 
-export default (props: Props) => {
+const NextLevelScreen = (props: Props) => {
   return (
-    <div>Next Level</div>
+    <div>Level {props.level} complete</div>
   );
 };
+
+const mapStateToProps = (state: {level: LevelState}) => {
+  const level = state.level.current;
+  const numberOfLevels = state.level.last
+  return { level, numberOfLevels };
+};
+const actionCreators = {};
+
+export default connect(mapStateToProps, actionCreators)(NextLevelScreen);
