86145278275f722e696123a2cbc1438db74ee970
Jeremy Green <j.green@jeremygreen.me.uk>
2018-12-08 18:10:01 +0000

    Fix new level screens.

--- src/redux/sagas/levelComplete.js
+++ src/redux/sagas/levelComplete.js
@@ -4,17 +4,17 @@ import type Action from 'redux';
 import { put, select, takeEvery } from 'redux-saga/effects';
 
 import { LEVEL_COMPLETE } from '../actionTypes';
-import { levelChange, gameComplete } from '../actions';
+import { levelChange, startLevel, gameComplete } from '../actions';
 import { isLastLevel } from '../selectors';
 
-export default function* nextLevel(): Generator<*,*,*> {
+export default function*(): Generator<*,*,*> {
   yield takeEvery(LEVEL_COMPLETE,
     function*(action: Action) {
       if (yield select(isLastLevel)) {
         yield put(gameComplete());
       } else {
         yield put(levelChange());
-        yield put(nextLevel());
+        yield put(startLevel());
       }
     });
 }
