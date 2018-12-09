100d43297a303709b775db2fb5680201d0b88a26
Jeremy Green <j.green@jeremygreen.me.uk>
2018-12-09 16:06:16 +0000

    Enable flow in all unit tests.

--- src/components/__tests__/Score.test.js
+++ src/components/__tests__/Score.test.js
@@ -1,3 +1,6 @@
+
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Score } from '../Score';
@@ -6,13 +9,13 @@ import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Score score="123"/>, div);
+  ReactDOM.render(<Score score={123}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders correctly', () => {
   const tree = renderer
-    .create(<Score score="123"/>)
+    .create(<Score score={123}/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
