0f7612283904493fb990ae9d00ce093189ebbe85
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-12-09 14:08:47 +0000

    More Util unit tests.

--- src/util/__tests__/Util.test.js
+++ src/util/__tests__/Util.test.js
@@ -1,7 +1,7 @@
-// @noflow
+// @flow
 import { Util } from '../Util'
 
-describe("clamp", () => {
+describe("clamp()", () => {
 
   it("doesn't change in-range value", () => {
     expect(Util.clamp(2, 1, 3)).toBe(2);
@@ -50,6 +50,25 @@ describe("clamp", () => {
 
   it("has max < min", () => {
     expect(() => Util.clamp(1, 3, 2)).toThrow("max of 2 is less than min 3 (with value of 1)");
+  });
+
+});
+
+describe("nonNull()", () => {
+
+  it("accepts all non-null values", () => {
+    // eslint-disable-next-line flowtype/no-weak-types
+    [0, 1, "", "foo", [], {}, Infinity, -Infinity, Number.NaN].forEach((value: any) => {
+      expect(Util.nonNull(value)).toBe(value);
+    });
+  });
+
+  it("rejects null", () => {
+    expect(() => Util.nonNull(null)).toThrow('null value.');
+  });
+
+  it("rejects null", () => {
+    expect(() => Util.nonNull(undefined)).toThrow('undefined value.');
   });
 
 });
