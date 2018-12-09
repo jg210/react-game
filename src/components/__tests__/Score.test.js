189679e61dcbf1eb2d07cfa94f02f3504acac77c
Jeremy Green <j.green@jeremygreen.me.uk>
2018-12-09 13:28:46 +0000

    Add jest snapshot test for Score component.

--- src/components/__tests__/Score.test.js
+++ src/components/__tests__/Score.test.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Score } from '../Score';
 
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Score score="123"/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Score score="123"/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
