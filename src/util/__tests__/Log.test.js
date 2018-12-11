8a889a6ac498ce79c6f3cd512fb61ea3485ffde5
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-12-11 22:33:33 +0000

    Better unit tests for Log.

--- src/util/__tests__/Log.test.js
+++ src/util/__tests__/Log.test.js
@@ -3,28 +3,32 @@ import { Log } from "../Log";
 
 describe("logging", () => {
 
+  const FAKE_DATE = new Date("2018-12-11T12:34:00z");
+  const T = FAKE_DATE.toISOString();
+  global.Date = jest.fn(() => FAKE_DATE);
+
   it("logs string at info level", () => {
     jest.spyOn(console, "log");
     Log.info("foo");
-    expect(console.log).toBeCalled();
+    expect(console.log).toBeCalledWith(`${T}  INFO foo`);
   });
 
   it("logs function at info level", () => {
     jest.spyOn(console, "log");
     Log.info(() => "foo");
-    expect(console.log).toBeCalled();
+    expect(console.log).toBeCalledWith(`${T}  INFO foo`);
   });
 
   it("logs string at debug level", () => {
     jest.spyOn(console, "log");
     Log.debug("bar");
-    expect(console.log).toBeCalled();
+    expect(console.log).toBeCalledWith(`${T} DEBUG bar`);
   });
 
   it("logs function at debug level", () => {
     jest.spyOn(console, "log");
     Log.debug(() => "bar");
-    expect(console.log).toBeCalled();
+    expect(console.log).toBeCalledWith(`${T} DEBUG bar`);
   });
 
 });
