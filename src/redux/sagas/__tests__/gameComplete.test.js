1bfb06eca0975fe9ce92bdf1f0d07530dd08f58e
Jeremy Green <j.green@jeremygreen.me.uk>
2018-12-12 21:04:06 +0000

    Add unit tests for gameComplete saga.

---
+++ src/redux/sagas/__tests__/gameComplete.test.js
@@ -0,0 +1,20 @@
+// @flow
+
+import { gameComplete, gameCompleteListener } from '../gameComplete';
+
+import { put, takeEvery } from 'redux-saga/effects';
+
+import { GAME_COMPLETE } from '../../actionTypes';
+import { screenChange } from '../../actions';
+
+it(`listens for ${GAME_COMPLETE}`, () => {
+  const generator = gameCompleteListener();
+  expect(generator.next().value).toEqual(takeEvery(GAME_COMPLETE, gameComplete));
+  expect(generator.next().done).toBe(true);
+});
+
+it('handles gameComplete', () => {
+  const generator = gameComplete();
+  expect(generator.next().value).toEqual(put(screenChange("gameComplete")));
+  expect(generator.next().done).toBe(true);
+});
