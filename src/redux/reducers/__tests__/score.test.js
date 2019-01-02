7368b21e46df4c0b2c441e32c961a990b701d680
Jeremy Green <j.green@jeremygreen.me.uk>
2019-01-02 22:14:07 +0000

    Unit test for score reducer.

---
+++ src/redux/reducers/__tests__/score.test.js
@@ -0,0 +1,9 @@
+// @flow
+
+import { score } from '../score'
+import { scoreUpdate } from '../../actions';
+
+it("tests score reducer", () => {
+  const state = score({ current: 3}, scoreUpdate(2));
+  expect(state).toEqual({ current: 5});
+});
