db569fd813eacf5c589debe072bf7409f1556f3f
Jeremy Green <j.green@jeremygreen.me.uk>
2019-01-03 23:44:58 +0000

    Coverage for levelComplete action creator.

--- src/redux/sagas/__tests__/levelComplete.test.js
+++ src/redux/sagas/__tests__/levelComplete.test.js
@@ -1,21 +1,31 @@
 // @flow
 
-import { levelComplete, levelCompleteListener } from '../levelComplete';
-
+import {
+  levelComplete as levelCompleteSaga,
+  levelCompleteListener
+} from '../levelComplete';
 import { put, select, takeEvery } from 'redux-saga/effects';
+import _ from 'lodash';
 
 import { LEVEL_COMPLETE } from '../../actionTypes';
-import { gameComplete, levelChange, startLevel } from '../../actions';
+import {
+  gameComplete,
+  levelChange,
+  levelComplete as levelCompleteAction,
+  startLevel
+} from '../../actions';
+
 import { isLastLevel } from '../../selectors';
+import { storeFactory } from '../../store';
 
 it(`listens for ${LEVEL_COMPLETE}`, () => {
   const generator = levelCompleteListener();
-  expect(generator.next().value).toEqual(takeEvery(LEVEL_COMPLETE, levelComplete));
+  expect(generator.next().value).toEqual(takeEvery(LEVEL_COMPLETE, levelCompleteSaga));
   expect(generator.next().done).toBe(true);
 });
 
 it('handles levelComplete for last level', () => {
-  const generator = levelComplete();
+  const generator = levelCompleteSaga();
   expect(generator.next().value).toEqual(select(isLastLevel));
   const lastLevel = true;
   expect(generator.next(lastLevel).value).toEqual(put(gameComplete()));
@@ -23,11 +33,21 @@ it('handles levelComplete for last level', () => {
 });
 
 it('handles levelComplete for last level', () => {
-  const generator = levelComplete();
+  const generator = levelCompleteSaga();
   expect(generator.next().value).toEqual(select(isLastLevel));
   const lastLevel = false;
   expect(generator.next(lastLevel).value).toEqual(put(levelChange()));
   expect(generator.next().value).toEqual(put(startLevel()));
   expect(generator.next().done).toBe(true);
+});
 
+it(`handles levelComplete action, testing by dispatching to a redux store instance`, () => {
+  const store = storeFactory();
+  const stateBefore = store.getState();
+  store.dispatch(levelCompleteAction());
+  const stateAfter = store.getState();
+  const stateAfterExpected = _.cloneDeep(stateBefore);
+  stateAfterExpected.level.current = 2;
+  stateAfterExpected.screen.current = "startLevel";
+  expect(stateAfter).toEqual(stateAfterExpected);
 });
