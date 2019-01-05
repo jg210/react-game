504030ea59bfb0c5c7e5de6e4c6b2a2b9c06b722
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-01-05 13:00:44 +0000

    Test Magnet's attachmentPosition() method.

--- src/engine/__tests__/Magnet.test.js
+++ src/engine/__tests__/Magnet.test.js
@@ -1,14 +1,21 @@
 import { Magnet } from '../Magnet';
 
-it("can be instantiated", () => {
+it("attachment position", () => {
   const world = {};
-  new Magnet({
-    x: 10,
-    y: 10,
+  const x = 10;
+  const y = 10;
+  const height = 5;
+  const magnet = new Magnet({
+    x: x,
+    y: y,
     minX: 5,
     maxX: 20,
     width: 5,
-    height: 5,
+    height: height,
     world: world
   });
+  const position = magnet.attachmentPosition();
+  expect(position.x).toBeCloseTo(x);
+  expect(position.y).toBeCloseTo(y + height / 2.0);
+  expect(Object.keys(position)).toEqual(["x", "y"]);
 });
