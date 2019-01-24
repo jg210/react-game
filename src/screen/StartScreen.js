f6399fb3ee1bf5d3a87274371d8d2b6f56f8b4fa
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-01-24 23:06:12 +0000

    Enter browser full screen mode when click on StartScreen.

--- src/screen/StartScreen.js
+++ src/screen/StartScreen.js
@@ -8,7 +8,8 @@ import { connect } from "react-redux";
 import { startGame } from '../redux/actions';
 
 type Props = {
-  startGame: () => void
+  startGame: () => void,
+  toggleFullscreen: () => void
 };
 
 export class StartScreen extends Component<Props> {
@@ -16,13 +17,18 @@ export class StartScreen extends Component<Props> {
   // eslint-disable-next-line flowtype/no-weak-types
   ref: { current: any } = React.createRef();
 
+  constructor(props: Props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   render() {
     return (
       <div
         ref={this.ref}
         tabIndex="0"
-        onClick={this.props.startGame}
-        onKeyPress={this.props.startGame}
+        onClick={this.handleClick}
+        onKeyPress={this.handleClick}
         className="StartScreen">
         <p>Press any key to start game.</p>
         <p>Use left and right arrow keys or the mouse to position the magnet.</p>
@@ -30,6 +36,12 @@ export class StartScreen extends Component<Props> {
         <p>You move to the next level when all objects have been dislodged.</p>
       </div>
     );
+  }
+
+  handleClick: () => void; // Allows binding in constructor without flow error.
+  handleClick() {
+    this.props.toggleFullscreen(); // Can only be called from UI event.
+    this.props.startGame();
   }
 
   componentDidMount() {
