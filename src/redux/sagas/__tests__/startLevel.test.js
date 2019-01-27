0091e28c40744409503020d8ee639f9a1545e592
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-01-27 10:49:55 +0000

    Add one to score when start new level.

--- src/redux/sagas/__tests__/startLevel.test.js
+++ src/redux/sagas/__tests__/startLevel.test.js
@@ -8,7 +8,7 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 
 import { START_LEVEL } from '../../actionTypes';
-import { screenChange } from '../../actions';
+import { scoreUpdate, screenChange } from '../../actions';
 it(`listens for ${START_LEVEL}`, () => {
   const generator = startLevelListener();
   expect(generator.next().value).toEqual(takeEvery(START_LEVEL, startLevel));
@@ -19,6 +19,7 @@ it('handles startLevel', () => {
   const generator = startLevel();
   expect(generator.next().value).toEqual(put(screenChange("startLevel")));
   expect(generator.next().value).toEqual(call(delay, 2500));
+  expect(generator.next().value).toEqual(put(scoreUpdate(1)));
   expect(generator.next().value).toEqual(put(screenChange("game")));
   expect(generator.next().done).toBe(true);
 });
