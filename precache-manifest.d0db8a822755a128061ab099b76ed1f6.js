0c0593dbd2ec6cbcd507bac73958ed8bcecc5a44
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-27 20:50:33 +0000

    Deploy jg210/react-game to github.com/jg210/react-game.git:gh-pages

--- precache-manifest.d0db8a822755a128061ab099b76ed1f6.js
+++ precache-manifest.d0db8a822755a128061ab099b76ed1f6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-game/static/js/runtime~main.cda9c7c5.js"
   },
   {
-    "revision": "3f99d11c2a1b29d1b3a9",
-    "url": "/react-game/static/js/main.3f99d11c.chunk.js"
+    "revision": "0e4e83c21891ef1f20e5",
+    "url": "/react-game/static/js/main.0e4e83c2.chunk.js"
   },
   {
     "revision": "2b870ffd35e598cdd037",
     "url": "/react-game/static/js/1.2b870ffd.chunk.js"
   },
   {
-    "revision": "3f99d11c2a1b29d1b3a9",
+    "revision": "0e4e83c21891ef1f20e5",
     "url": "/react-game/static/css/main.2a7ad902.chunk.css"
   },
   {
-    "revision": "c93d5a2923d14eaa7ff892e35d66178c",
+    "revision": "ef74617b911025bf07685f5675f34631",
     "url": "/react-game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-game/precache-manifest.db8b9d017daa3a15e43916ce9345efd3.js"
+  "/react-game/precache-manifest.d0db8a822755a128061ab099b76ed1f6.js"
 );
 
 workbox.clientsClaim();
