86e7f29735c53dc22df88b3baecb3aec34acdfc4
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-01-27 18:52:20 +0000

    Unit test clicking on StartLevelScreen.

--- src/screen/__tests__/StartLevelScreen.test.js
+++ src/screen/__tests__/StartLevelScreen.test.js
@@ -6,14 +6,25 @@ import { StartLevelScreen } from '../StartLevelScreen';
 
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 
 it('renders correctly', () => {
   const handler = jest.fn();
-  const tree = renderer
-    .create(<StartLevelScreen level={3} dismissStartLevelScreen={handler}/>)
-    .toJSON();
+  const tree = renderer.create(
+    <StartLevelScreen level={3} dismissStartLevelScreen={handler}/>
+  ).toJSON();
   expect(tree).toMatchSnapshot();
   expect(handler).toHaveBeenCalledTimes(0);
 });
 
-// TODO Test click event.
+it('sends event when clicked on', () => {
+  const handler = jest.fn();
+  const component = mount(<StartLevelScreen
+    level={2}
+    dismissStartLevelScreen={handler}/>
+  );
+  const div = component.find('div');
+  const domNode = div.getDOMNode();
+  div.simulate('click', { target: domNode });
+  expect(handler).toHaveBeenCalledTimes(1);
+});
