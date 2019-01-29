dff0f33bf02d06226ad2914b1251bf8dda3faf83
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-01-29 22:31:33 +0000

    Handle START action properly during startLevel saga.

--- src/redux/sagas/__tests__/startLevel.test.js
+++ src/redux/sagas/__tests__/startLevel.test.js
@@ -7,7 +7,7 @@ import { startLevel, startLevelListener } from '../startLevel';
 import { call, put, race, take, takeEvery } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 
-import { START_LEVEL, DISMISS_START_LEVEL_SCREEN } from '../../actionTypes';
+import { START_LEVEL, DISMISS_START_LEVEL_SCREEN, START } from '../../actionTypes';
 import { scoreUpdate, screenChange } from '../../actions';
 it(`listens for ${START_LEVEL}`, () => {
   const generator = startLevelListener();
@@ -20,8 +20,17 @@ it('handles startLevel', () => {
   expect(generator.next().value).toEqual(put(screenChange("startLevel")));
   const sleep = call(delay, 2500);
   const dismissal = take(DISMISS_START_LEVEL_SCREEN);
-  expect(generator.next().value).toEqual(race({sleep, dismissal}));
-  expect(generator.next().value).toEqual(put(scoreUpdate(1)));
+  expect(generator.next().value).toEqual(race({sleep, dismissal, cancel: take(START)}));
+  expect(generator.next({cancel: undefined}).value).toEqual(put(scoreUpdate(1)));
   expect(generator.next().value).toEqual(put(screenChange("game")));
   expect(generator.next().done).toBe(true);
+});
+
+it('handles startLevel with START cancelling the saga', () => {
+  const generator = startLevel();
+  expect(generator.next().value).toEqual(put(screenChange("startLevel")));
+  const sleep = call(delay, 2500);
+  const dismissal = take(DISMISS_START_LEVEL_SCREEN);
+  expect(generator.next().value).toEqual(race({sleep, dismissal, cancel: take(START)}));
+  expect(generator.next({cancel: true}).done).toBe(true);
 });
--- src/redux/sagas/startLevel.js
+++ src/redux/sagas/startLevel.js
@@ -5,16 +5,22 @@
 import { call, put, race, take, takeEvery } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 
-import { START_LEVEL, DISMISS_START_LEVEL_SCREEN } from '../actionTypes';
+import { START_LEVEL, DISMISS_START_LEVEL_SCREEN, START } from '../actionTypes';
 import { scoreUpdate, screenChange } from '../actions';
 
 export function* startLevel(): Generator<*,*,*> {
   yield put(screenChange("startLevel"));
-  const sleep = call(delay, 2500);
-  const dismissal = take(DISMISS_START_LEVEL_SCREEN);
-  yield race({sleep, dismissal});
-  yield put(scoreUpdate(1));
-  yield put(screenChange("game"));
+  const { cancel } = yield race({
+    // With no user interaction, show the startLevel screen for 2.5s.
+    sleep: call(delay, 2500),
+    // Clicking on the startLevel screen starts the level immediately.
+    dismissal: take(DISMISS_START_LEVEL_SCREEN),
+    // It's also possible to restart the game by exiting full screen mode.
+    cancel: take(START)});
+  if (!cancel) {
+    yield put(scoreUpdate(1));
+    yield put(screenChange("game"));
+  }
 }
 
 export function* startLevelListener(): Generator<*,*,*> {
