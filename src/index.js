9ab70d83a07306a24ff3a36cbfdbfe74b7c1bfc6
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-07-19 21:54:51 +0100

    Configure sentry crash reporting.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from '@sentry/browser';
 import App from './components/App';
 import { Util } from './util/Util'
 
 // https://www.w3.org/TR/pointerevents/ polyfill.
 import 'pepjs';
+
+// Crash reporting - should be configured as early as possible.
+const SENTRY_DSN = "__SENTRY_DSN__";
+// eslint-disable-next-line no-useless-concat
+if (SENTRY_DSN !== "__SENTRY" + "_DSN__") {
+  Sentry.init({dsn: SENTRY_DSN});
+}
 
 // https://github.com/liabru/matter-js/issues/559
 window.decomp = require('poly-decomp');
