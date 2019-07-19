018368f9ddcf90e978be190d6b328d4709898105
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-07-19 22:30:16 +0100

    Crash app when toggling debug mode to test out sentry.

--- src/redux/reducers/debug.js
+++ src/redux/reducers/debug.js
@@ -17,7 +17,8 @@ const initialState: DebugState = {
 export const debug = (state: DebugState = initialState, action: Action): DebugState => {
   switch (action.type) {
     case TOGGLE_WIREFRAME_MODE: {
-      return {...state, wireframe: !state.wireframe};
+      throw new Error("testing 123");
+      //return {...state, wireframe: !state.wireframe};
     }
     default: {
       return state;
