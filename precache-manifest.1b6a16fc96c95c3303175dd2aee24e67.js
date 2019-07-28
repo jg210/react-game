939bc26545b5f219dee673aaa4536c622b4b08de
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-28 11:51:28 +0000

    Deploy jg210/react-game to github.com/jg210/react-game.git:gh-pages

---
+++ precache-manifest.1b6a16fc96c95c3303175dd2aee24e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b21343aa673b4b4e17e9",
+    "url": "/react-game/static/js/runtime~main.b21343aa.js"
+  },
+  {
+    "revision": "69ce8a3a82a2f3566cdd",
+    "url": "/react-game/static/js/main.69ce8a3a.chunk.js"
+  },
+  {
+    "revision": "804b2244185bfb33b078",
+    "url": "/react-game/static/js/2.804b2244.chunk.js"
+  },
+  {
+    "revision": "69ce8a3a82a2f3566cdd",
+    "url": "/react-game/static/css/main.be3cf3a3.chunk.css"
+  },
+  {
+    "revision": "7bf20320b6a1708102a25869197e1776",
+    "url": "/react-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-game/precache-manifest.d2d32cce5f0901f6516ae175e176facb.js"
+  "/react-game/precache-manifest.1b6a16fc96c95c3303175dd2aee24e67.js"
 );
 
 workbox.clientsClaim();
