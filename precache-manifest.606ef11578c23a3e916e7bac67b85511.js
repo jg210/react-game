c179a566b775718a37b17488c9b9b85958501c9f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-02 20:07:38 +0000

    Deploy jg210/react-game to github.com/jg210/react-game.git:gh-pages

---
+++ precache-manifest.606ef11578c23a3e916e7bac67b85511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71cecb97dd3b55421842025a08aca233",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "a2acba204114c9756c5a",
+    "url": "/react-game/static/css/main.ef067600.chunk.css"
+  },
+  {
+    "revision": "aa3ab066fa5cc90e9cc0",
+    "url": "/react-game/static/js/2.66c57ce5.chunk.js"
+  },
+  {
+    "revision": "e1cad8e8f8d54c9be5fed03834884dd6",
+    "url": "/react-game/static/js/2.66c57ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2acba204114c9756c5a",
+    "url": "/react-game/static/js/main.05ae66f6.chunk.js"
+  },
+  {
+    "revision": "c9538823fa95012b9746",
+    "url": "/react-game/static/js/runtime-main.78fbfa5d.js"
+  }
+]);
